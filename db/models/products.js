d2241033fa0603cb689eaa48166cb2c851ff2e88
Oretastic <hloreair@outlook.com>
2020-03-25 22:19:39 -0400

    fixed something i broke

--- db/models/products.js
+++ db/models/products.js
@@ -5,10 +5,13 @@ const products = {
     return (await client.query("SELECT * from products")).rows;
   },
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
   create: async ({ name, price, image }) => {
     const SQL = `INSERT INTO products(name, price, image) values($1, $2, $3) returning *`;
     return (await client.query(SQL, [name, price, image])).rows[0];
 =======
+=======
+>>>>>>> Stashed changes
   create: async ({ name, price, image, department, material, adjective }) => {
     const SQL = `INSERT INTO products(name, price, image, department, material, adjective) values($1, $2, $3, $4, $5, $6) returning *`;
     return (
