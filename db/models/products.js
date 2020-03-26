a94f968ebe2e7ef5d99d74578994c651332d9dfa
Oretastic <hloreair@outlook.com>
2020-03-25 22:18:25 -0400

    inventory-controll finished

--- db/models/products.js
+++ db/models/products.js
@@ -4,9 +4,24 @@ const products = {
   read: async () => {
     return (await client.query("SELECT * from products")).rows;
   },
+<<<<<<< Updated upstream
   create: async ({ name, price, image }) => {
     const SQL = `INSERT INTO products(name, price, image) values($1, $2, $3) returning *`;
     return (await client.query(SQL, [name, price, image])).rows[0];
+=======
+  create: async ({ name, price, image, department, material, adjective }) => {
+    const SQL = `INSERT INTO products(name, price, image, department, material, adjective) values($1, $2, $3, $4, $5, $6) returning *`;
+    return (
+      await client.query(SQL, [
+        name,
+        price,
+        image,
+        department,
+        material,
+        adjective
+      ])
+    ).rows[0];
+>>>>>>> Stashed changes
   },
   changeInventory : async(productId, total, op) => {
     if(op === '-') {
