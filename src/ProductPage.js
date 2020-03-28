08f4a31859f78a1bade6e2987eac247d33975ea9
Robert Celestino <panda@Roberts-MacBook-Air-2.local>
2020-03-28 12:13:03 -0400

    product page

--- src/ProductPage.js
+++ src/ProductPage.js
@@ -6,9 +6,17 @@ const ProductPage = ({ product, addToCart }) => {
       <img src={product.image} />
       <h1>{product.name}</h1>
       <p className="price">Price: ${Number(product.price).toFixed(2)}</p>
-      <p>{product.department}</p>
-      <p>{product.adjective}</p>
-      <p>{product.material}</p>
+      <h4>{product.department}</h4>
+      <h4>Description</h4>
+      <p>This awesome {product.adjective} product!</p>
+      <p>
+        Made from {product.material} Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+        in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+        pariatur.
+      </p>
       <button onClick={() => addToCart(product.id)}>Add to Cart</button>
     </div>
   );
