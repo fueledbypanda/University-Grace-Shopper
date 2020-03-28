7207439c4edd1d916a5e35fc712032a8df1828e8
Brandon Nall <firewalkwithb@gmail.com>
2020-03-28 12:04:19 -0400

    commit

--- src/Products.js
+++ src/Products.js
@@ -1,7 +1,13 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Products = ({ products, addToCart, setProductView, save, lowerInventory }) => {
+const Products = ({
+  products,
+  addToCart,
+  setProductView,
+  save,
+  lowerInventory,
+}) => {
   return (
     <div id="products">
       <h1>Products</h1>
@@ -17,21 +23,24 @@ const Products = ({ products, addToCart, setProductView, save, lowerInventory })
                 <img src={product.image} />
                 <div>${Number(product.price).toFixed(2)}</div>
                 <div>
-                  {
-                    product.inventory <= 0 ? 
-                      <p>Out Of Stock</p> : 
-                    product.inventory > 0 && product.inventory < 15 ?
-                      <p>Limited Stock</p> :
+                  {product.inventory <= 0 ? (
+                    <p>Out Of Stock</p>
+                  ) : product.inventory > 0 && product.inventory < 15 ? (
+                    <p>Limited Stock</p>
+                  ) : (
                     <p>In Stock</p>
-                  }
+                  )}
                 </div>
               </Link>
-              <button onClick={() => {
-                addToCart(product.id)
-                lowerInventory(product.id)
-              }}>Add to Cart</button>
-              <button onClick={()=> save(product.id)}>Save For Later</button>
-
+              <button
+                onClick={() => {
+                  addToCart(product.id);
+                  lowerInventory(product.id);
+                }}
+              >
+                Add to Cart
+              </button>
+              <button onClick={() => save(product.id)}>Save For Later</button>
             </li>
           );
         })}
